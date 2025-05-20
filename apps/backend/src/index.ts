1abaa6d89c41cdc27a944204fb93eadd2287d330
Ranjan N <ranjann2004@gmail.com>
2025-05-20 18:58:43 +0530

    Added auth routes
    Signed-off-by: Ranjan N <ranjann2004@gmail.com>

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,118 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { signinSchema, signupSchema } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
+import { JWT_SECRET, SALT_ROUNDS } from "@repo/common-backend/config";
+import bcrypt from "bcryptjs";
 
 const app = express();
+
+app.use(express.json());
+
+app.post("/signup", async (req: Request, res: Response) => {
+  try {
+    const result = signupSchema.safeParse(req.body);
+
+    if (!result.success) {
+      res.status(400).json({
+        error: result.error.format(),
+      });
+      return;
+    }
+
+    const existingUser = await prismaClient.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+    });
+
+    if (existingUser) {
+      res.status(400).json({
+        message: "User already exists",
+      });
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(result.data.password, SALT_ROUNDS);
+
+    const user = await prismaClient.user.create({
+      data: {
+        email: result.data.email,
+        password: hashedPassword,
+        name: result.data.name,
+      },
+    });
+
+    const token = jwt.sign(
+      {
+        userId: user.id,
+      },
+      JWT_SECRET
+    );
+
+    res.status(200).json({
+      token,
+    });
+  } catch (error) {
+    console.error("Error signing up user:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
+
+app.post("/signin", async (req: Request, res: Response) => {
+  try {
+    const result = signinSchema.safeParse(req.body);
+
+    if (!result.success) {
+      res.status(400).json({
+        error: result.error.format(),
+      });
+      return;
+    }
+
+    const user = await prismaClient.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+    });
+
+    if (!user) {
+      res.status(401).json({
+        message: "User not found",
+      });
+      return;
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      result.data.password,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      res.status(401).json({
+        message: "Invalid password",
+      });
+      return;
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user.id,
+      },
+      JWT_SECRET
+    );
+    res.status(200).json({
+      token,
+    });
+  } catch (error) {
+    console.error("Error signing in user:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
