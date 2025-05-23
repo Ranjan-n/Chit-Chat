a85f5c77df1e5b7535ffef5f85f37a87e7f943da
Ranjan N <ranjann2004@gmail.com>
2025-05-23 16:14:16 +0530

    Added redis config
    Signed-off-by: Ranjan N <ranjann2004@gmail.com>

--- packages/common-backend/src/index.ts
+++ packages/common-backend/src/index.ts
@@ -1,2 +1,7 @@
 export const JWT_SECRET = process.env.JWT_SECRET || "SECRET123";
 export const SALT_ROUNDS = process.env.SALT_ROUNDS || "10";
+
+export const redisconnection = {
+  host: process.env.REDIS_HOST || "localhost",
+  port: parseInt(process.env.REDIS_PORT || "6379"),
+};
