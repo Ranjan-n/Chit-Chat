6b11f55c933c6df768b220da7f2c6914276f4b03
Ranjan N <ranjann2004@gmail.com>
2025-05-24 17:37:08 +0530

    Added type inference
    Signed-off-by: Ranjan N <ranjann2004@gmail.com>

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -6,10 +6,14 @@ export const signupSchema = z.object({
   name: z.string().min(3).max(20),
 });
 
+export type SignUpInput = z.infer<typeof signupSchema>;
+
 export const signinSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
 });
+
+export type SignInInput = z.infer<typeof signinSchema>;
 
 export const createRoomSchema = z.object({
   name: z.string().min(1),
