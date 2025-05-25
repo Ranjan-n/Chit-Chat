fa475e3f36f8d7d8a721177b9bb77d33fd2cc168
Ranjan N <ranjann2004@gmail.com>
2025-05-25 19:07:07 +0530

    Added URL config for WEBSOCKET server
    Signed-off-by: Ranjan N <ranjann2004@gmail.com>

--- apps/frontend/app/config.ts
+++ apps/frontend/app/config.ts
@@ -1,1 +1,5 @@
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
+export const BACKEND_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
+
+export const WEBSOCKET_URL =
+  process.env.NEXT_PUBLIC_WEBSOCKET_URL || "ws://localhost:8080";
