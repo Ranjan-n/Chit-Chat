1363c6992eab4f3804fca0f1df2d10bd355ed9e6
Ranjan N <ranjann2004@gmail.com>
2025-05-26 17:44:20 +0530

    Updated config file with dotenv config
    Signed-off-by: Ranjan N <ranjann2004@gmail.com>

--- packages/common-backend/src/index.ts
+++ packages/common-backend/src/index.ts
@@ -1,7 +1,15 @@
+import dotenv from "dotenv";
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: resolve(__dirname, "../../../.env") });
+
 export const JWT_SECRET = process.env.JWT_SECRET || "SECRET123";
 export const SALT_ROUNDS = process.env.SALT_ROUNDS || "10";
 
 export const redisconnection = {
-  host: process.env.REDIS_HOST || "localhost",
-  port: parseInt(process.env.REDIS_PORT || "6379"),
+  url: process.env.REDIS_URL || "redis://localhost:6379"
 };
